d526db85dfb46cbc36fbc78aff1a586248468480
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-11-03 10:56:30 +0100

    feat: ajout job 1, jour 1

---
+++ jour1/job1/main.cpp
@@ -0,0 +1,15 @@
+/*
+* Auteur : Lorenzo OTTAVIANI.
+* Date : 03/11/2025 10h50
+* But du programme :
+*    Affiche Hello world dans le terminal.
+* Entrée : ∅
+* Sortie : Affichage d'Hello World.
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
